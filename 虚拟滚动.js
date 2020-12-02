d0cc5e198ac2f3c0caa3757562429f451d86c700
thomas <caianfa@qq.com>
2020-12-02 17:12:50 +0800

    add virtual scroll

---
+++ 虚拟滚动.js
@@ -0,0 +1,13 @@
+// https://juejin.cn/post/6844903982742110216  「前端进阶」高性能渲染十万条数据(虚拟列表)
+
+
+// resizeobserver
+
+// IntersectionObserver
+
+// https://juejin.cn/post/6844904185830473736  这个长列表优化渲染，值得学一波（通俗易懂，学不懂你找我）
+
+
+// react 开源库  react-window  react-virtualized  virtual-list(antd)
+// react-window 是 react-virtualized的重写， api更加友好，也解决了很多问题
+// 浏览器实现  virtual-scroller https://www.chromestatus.com/feature/5673195159945216
